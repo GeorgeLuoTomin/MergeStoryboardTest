bb3ee6986325b91c5a74fbcd2d9c7007a5a7694f
James Shih <jamesshih@tomingroup.com>
2015-08-12 11:53:29 +0800

    更改畫面

---
+++ MergeStoryboardTest/MergeStoryboardTest/ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  MergeStoryboardTest
+//
+//  Created by James on 2015/8/12.
+//  Copyright (c) 2015年 tomin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController
+
+@end
